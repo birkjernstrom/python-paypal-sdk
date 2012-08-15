b0420798b88b3c642d5175abb69235d7c2c16f83
Birk Nilson <me@birknilson.se>
2012-08-15 21:50:42 +0200

    Introducing paypal.ext.tornado_client with AsyncClient

---
+++ paypal/ext/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['tornado_client']
+
+from paypal.ext import tornado_client
---
+++ paypal/ext/tornado_client.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from tornado import gen
+from tornado.httpclient import AsyncHTTPClient
+
+from paypal import client
+
+
+class AsyncClient(client.Client):
+    @gen.engine
+    def __call__(self,
+                 method,
+                 endpoint=None,
+                 payload=None,
+                 response_as_dict=False,
+                 callback=None,
+                 **params):
+        """
+        """
+        endpoint = endpoint if endpoint else self.config.endpoint
+        if payload is not None:
+            params.update(payload)
+
+        headers = self.get_headers()
+        request = self.generate_request(method, params)
+        encoded_request = request.encode()
+        group_id = self.generate_group_id()
+
+        self.log_api_request(encoded_request, request, group_id=group_id)
+
+        httpclient = AsyncHTTPClient()
+        response = yield gen.Task(httpclient.fetch, endpoint,
+                                  method='POST', body=encoded_request,
+                                  headers=headers)
+
+        encoded = response.body
+        r = self.generate_response(method, encoded, as_dict=response_as_dict)
+        self.log_api_response(encoded, r, group_id=group_id)
+        if callback:
+            callback(r)
