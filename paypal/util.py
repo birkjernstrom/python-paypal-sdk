3d30c656509ee45d94d51f66d5769b6b83d8cb63
Birk Nilson <me@birknilson.se>
2012-08-15 16:38:00 +0200

    Introduced logging in paypal.util

--- paypal/util.py
+++ paypal/util.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
+#: Main logger utilized across the entire library
+logger = logging.getLogger('paypal.core')
+#: Restricted logger which is intended to only log API
+#: requests and responses along with potential errors
+#: associated with them.
+api_logger = logging.getLogger('paypal.api')
+
+
+class LoggingNullHandler(logging.Handler):
+    """Logging handler which emits nothing."""
+    def emit(self, record):
+        pass
+
+
+logger.addHandler(LoggingNullHandler())
+api_logger.addHandler(LoggingNullHandler())
+
 
 def ensure_unicode(obj):
     """Recursively encode the given ``obj`` to unicode.
