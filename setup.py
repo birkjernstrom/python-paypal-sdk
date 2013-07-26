98f78317680335a212bdf6dbb4e0d201965c0815
Martin Melin <martin@tictail.com>
2013-07-26 23:21:35 +0200

    Add a rudimentary setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='paypal',
+    version='0.0.1',
+    packages=['paypal', ],
+    author='Birk Nilson',
+    author_email='birk@tictail.com',
+    url='http://github.com/birknilson/python-paypal-sdk',
+)
